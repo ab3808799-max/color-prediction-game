f0764a13088f37efbb65265e994418855b6de1b6
ab3808799-max <ab3808799@gmail.com>
2026-09-07 23:50:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
- document. addEventListener('DOMContentLoaded', function()
+ document . getElementByI.addEventListener . click, function . alert Start button clicked 
+
+document. addEventListener('DOMContentLoaded', function()
   { alert('Welcome to Color Prediction Demo');
    console. log('Game loaded successfully');
    });
